66fe6f8db247d5b4fec4b03869c65ced5702a7af
fajar abdillah ahmad <fajardillah25@gmail.com>
2022-09-09 22:57:47 +0700

    belajar MathClass

---
+++ scr/MathClass.java
@@ -0,0 +1,32 @@
+
+public class MathClass {
+    /*
+    ada banyak sekali static method untuk operasi numerik seperti trigono,logarithm
+     */
+    public static void main(String[] args) {
+        /*
+        * Math.min(int a, int b) digunakan mengambil nilai minimal dari 2 nilai
+         */
+        var min = Math.min(12141,12143);
+        System.out.println(min);
+
+        /*
+        * Math.max(int a, int b) digunakan untuk mengambil data maximal dari 2 integer
+         */
+        var max = Math.max(14512,5431);
+        System.out.println(max);
+
+        /*
+        * 
+         */
+        var x = Math.abs(324);
+        System.out.println(x);
+
+        /*
+        * Math.cos(value) digunakan untuk mengambil nilai cos
+         */
+        var z = Math.cos(90);
+        System.out.println(z);
+
+    }
+}
