d51f9d780f662a0606ba79e06d5a024220334e48
fajar abdillah ahmad <fajardillah25@gmail.com>
2022-09-12 23:45:37 +0700

    menambah baris untuk membuat file properties baru

--- scr/PropertiesAplication.java
+++ scr/PropertiesAplication.java
@@ -1,10 +1,11 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.Properties;
 
 public class PropertiesAplication {
     public static void main(String[] args) {
+        /*
+        dibawah ini adalah cara mengakses field yang ada pada ClassProperties
+         */
         try{
             Properties properties = new Properties();
             properties.load(new FileInputStream("application.properties"));
@@ -16,6 +17,21 @@ public class PropertiesAplication {
             System.out.println("file tidak ditemukan");
         }catch (IOException exception){
             System.out.println("gagal load data properties");
+        }
+
+        /*
+        dibawah ini adalah cara membuat PropertiesClass yang baru
+         */
+        try{
+            Properties properties = new Properties();
+            properties.put("database.name","KlinikCeria");
+            properties.put("database.port","1493");
+            properties.put("database.email","fajardillah25@gmail.com");
+            properties.store(new FileOutputStream("DATABASE.properties"),"");
+        }catch (FileNotFoundException exception){
+            System.out.println("error membuat file properties baru");
+        }catch (IOException exception){
+            System.out.println("error menyimpan file properties baru");
         }
 
     }
